66c9e467f37a794f2079fc742a48ef22be56fbb5
Ximahxx <salmadear8@gmail.com>
2022-12-10 15:08:57 -0800

    Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (j > i)
+			{
+				for (k = 0; k < 10; k++)
+				{
+					if (k > j)
+					{
+						putchar(i + '0');
+						putchar(j + '0');
+						putchar(k + '0');
+						if (i < 8)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
