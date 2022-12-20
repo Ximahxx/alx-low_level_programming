f14d24b464e3c8a8f68ad4b235d2fb57b5536257
Ximahxx <salmadear8@gmail.com>
2022-12-19 18:44:53 -0800

    omo

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "_putchar.c"
 /**
  * _puts - prints a string followed by a new line..
  * @str: the string to print.
@@ -15,7 +14,10 @@ void _puts(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		char x;
+
+		x = str[i];
+		_putchar(x);
 	}
 	_putchar('\n');
 }
